b700cdad933fd048252c93c1bbbc7c667bae5d78
Shashin Asiri <shashin.asiri@gmail.com>
2023-05-21 19:44:20 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path='/' Component={Homepage} />
+        <Route path='/hats' Component={HatsPage} />
+      </Routes>
     </div>
   );
 }
