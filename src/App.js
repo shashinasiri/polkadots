74036aae4d230d94d971e648410cda276b34ded9
Shashin Asiri <shashin.asiri@gmail.com>
2023-05-21 20:55:29 +0530

    Added Shop page and styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import './App.css';
 
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path='/' Component={Homepage} />
-        <Route path='/hats' Component={HatsPage} />
+        <Route path='/shop' Component={ShopPage} />
       </Routes>
     </div>
   );
